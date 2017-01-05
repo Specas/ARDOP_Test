83729ed909247e415a70cda40743ab9132949644
karunthewarrior <karunthewarrior@gmail.com>
2017-01-05 21:39:47 +0530

    Forward Propagation Test

--- nn.h
+++ nn.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <armadillo>
+#include <vector>
 
 using namespace std;
 using namespace arma;
@@ -14,3 +15,63 @@ mat sigmoid(mat in)
 	out = 1/out;
 	return out;
 }
+
+vector<mat> init_weights(mat nodes)
+{
+	vector<mat> unrolled_weights;
+	mat temp;
+	for(int i=0;i<nodes.n_cols-1;i++)
+	{
+		temp=randu<mat>(nodes(i+1),nodes(i)+1);
+		unrolled_weights.push_back(temp);		
+	}
+	return unrolled_weights;	
+}
+
+mat forward_prop(mat input, vector<mat> weights, mat nodes)
+{
+	mat one;
+	one<<1;
+	int n_layers = nodes.n_cols;	
+	for(int i=0;i<n_layers-1;i++)
+	{
+		weights[i].print("weights");
+		input = join_horiz(one,input);
+		input = sigmoid(input * trans(weights[i])); 
+		input.print();
+	}
+	return input;
+}
+
+
+
+// for (int i = 1; i < argc; ++i)
+//     {
+//         if (string(argv[i]) == "--cascade")
+//             cascadeName = argv[++i];
+//         else if (string(argv[i]) == "--video")
+//         {
+//             inputName = argv[++i];
+//             isInputVideo = true;
+//         }
+//         else if (string(argv[i]) == "--camera")
+//         {
+//             inputName = argv[++i];
+//             isInputCamera = true;
+//         }
+//         else if (string(argv[i]) == "--help")
+//         {
+//             help();
+//             return -1;
+//         }
+//         else if (!isInputImage)
+//         {
+//             inputName = argv[i];
+//             isInputImage = true;
+//         }
+//         else
+//         {
+//             cout << "Unknown key: " << argv[i] << endl;
+//             return -1;
+//         }
+//     }
--- test.cpp
+++ test.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <armadillo>
+#include <vector>
 #include "nn.h"
 using namespace std;
 using namespace arma;
 
 int main()
 {
-	mat A,B;
-	A<<0<<0<<0;
-	B = sigmoid(A);
-	B.print();
+	mat in,out,nodes; 
+	in<<1<<3;
+	nodes<<2<<2<<1;
+	vector<mat> w;
+	w = init_weights(nodes);
+	out = forward_prop(in,w,nodes);
+	out.print();
+
 	return 0;
 }
+
